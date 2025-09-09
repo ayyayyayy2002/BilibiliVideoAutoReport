75b4e848b17ddc2c434e3f0d3f417c257ab8c600
ayyayyayy2002 <199196441+ayyayyayy2002@users.noreply.github.com>
2025-09-09 09:27:40 +0800

    Update main_report.py

--- main_report.py
+++ main_report.py
@@ -54,7 +54,7 @@ def report():
     }
     tids = list(tids_with_weights.keys())
     weights = list(tids_with_weights.values())
-    driver = start_chrome(user_data_dir, False, proxy)
+    driver = start_chrome(user_data_dir, True, proxy)
     try:
         with open(uid_file, 'r', encoding='utf-8') as file:  # 以读取模式打开文件
             for line in file:
@@ -101,7 +101,7 @@ def report():
             titles.append(archive['title'])
             pics.append(archive['pic'])
         count = len(aids)
-        if count == 0:
+        if count == 0 or name=="账号已注销":
             try:
                 with open(black_file, 'r', encoding='utf-8') as f:
                     lines = f.readlines()
