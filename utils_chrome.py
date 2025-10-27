3abe53d547b7fa87d0b445c070f63f3f6059de4a
ayyayyayy2002 <199196441+ayyayyayy2002@users.noreply.github.com>
2025-10-27 22:53:01 +0800

    Update utils_chrome.py

--- utils_chrome.py
+++ utils_chrome.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
@@ -6,24 +7,25 @@ from variables import chrome_binary_path, chrome_driver_path
 
 
 def start_chrome(headless: bool = False, proxy_url: str = None):
+    user_data_dir =  "User Data"
+    if os.path.exists(user_data_dir):
+        shutil.rmtree(user_data_dir)
 
     options = webdriver.ChromeOptions()
     options.binary_location = chrome_binary_path
+    options.add_argument(f'--user-data-dir={user_data_dir}')
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_argument("--disable-gpu")
     options.add_argument("--disable-sync")
     options.add_argument("disable-cache")
     options.add_argument('log-level=3')
 
-
-    # 代理
     if proxy_url:
         options.add_argument(f'--proxy-server={proxy_url}')
     else:
         options.add_argument('--proxy-server="direct://"')
         options.add_argument('--proxy-bypass-list=*')
 
-    # 无头
     if headless:
         options.add_argument("--headless=new")
 
@@ -31,6 +33,7 @@ def start_chrome(headless: bool = False, proxy_url: str = None):
     driver = webdriver.Chrome(service=service, options=options)
     driver.set_window_size(1000, 700)
     return driver
+
 
 
 # 示例调用
