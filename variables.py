55cbf24ca34bc7b136e7b226c1d2f91609bede00
ayyayyayy2002 <199196441+ayyayyayy2002@users.noreply.github.com>
2026-01-15 15:36:16 +0800

    Update variables.py

--- variables.py
+++ variables.py
@@ -8,7 +8,7 @@ collector_cookie_file = os.path.join(base_dir,'model', 'collector.pkl')
 UA='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
 reason='标题title和封面违规,黑产,引流'
 group='哔哩哔哩'
-user_data_dir = os.path.join(base_dir,  "User Data")
+user_data_dir = os.path.join(base_dir,"list",  "User Data")
 uid_file = os.path.join(base_dir, 'list', 'uid.txt')
 white_file = os.path.join(base_dir, 'list', 'white.txt')
 black_file = os.path.join(base_dir, 'list', 'black.txt')
@@ -18,7 +18,6 @@ save_dir = os.path.join(base_dir, "record", "pair")
 chrome_binary_path = os.path.join(base_dir, 'list','chrome-win', 'chrome.exe')
 chrome_driver_path = os.path.join(base_dir, 'li','chrome-win', 'chromedriver.exe')
 CLASH_API_URL = "http://127.0.0.1:9090"
-
 proxies = {
     'http': "http://127.0.0.1:7890",
     'https': "http://127.0.0.1:7890"
