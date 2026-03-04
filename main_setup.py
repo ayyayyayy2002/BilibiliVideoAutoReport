a0c4e3d2809cecd3e37d285c825ff937840f6ceb
ayyayyayy2002 <199196441+ayyayyayy2002@users.noreply.github.com>
2026-03-04 14:49:52 +0800

    Update main_setup.py

--- main_setup.py
+++ main_setup.py
@@ -1,27 +1,9 @@
 import os
 from utils_chrome import start_chrome
-from variables import reporter_cookie_file, collector_cookie_file, CLASH_PROXY_URL
+from variables import collector_cookie_file, CLASH_PROXY_URL, accountcount
 
 
 def setup():
-    # ------------------- Reporter 登录 -------------------
-    print("登录Reporter")
-    # 如果文件存在就传入 storage_state，否则为 None
-    storage_state = reporter_cookie_file if os.path.exists(reporter_cookie_file) else None
-    playwright, browser, context, page = start_chrome(headless=False, storage_state=storage_state,proxy_url=CLASH_PROXY_URL)
-    page.goto("https://space.bilibili.com")
-
-    page.wait_for_selector(".nickname", timeout=300000)
-    nickname = page.query_selector(".nickname").inner_text()
-    print("已登录，昵称:", nickname)
-
-    # 保存最新状态
-    context.storage_state(path=reporter_cookie_file)
-
-    context.close()
-    browser.close()
-    playwright.stop()
-
     # ------------------- Collector 登录 -------------------
     print("登录Collector")
     storage_state = collector_cookie_file if os.path.exists(collector_cookie_file) else None
@@ -42,3 +24,23 @@ def setup():
     context.close()
     browser.close()
     playwright.stop()
+    # ------------------- Reporter 登录 -------------------
+    for i in range(0, accountcount):
+        print(f"登录Reporter{i+1}")
+        # 如果文件存在就传入 storage_state，否则为 None
+        storage_state = os.path.join('model', f'reporter{i}.json')  if os.path.exists(os.path.join('model', f'reporter{i}.json') ) else None
+        playwright, browser, context, page = start_chrome(headless=False, storage_state=storage_state,proxy_url=CLASH_PROXY_URL)
+        page.goto("https://space.bilibili.com")
+
+        page.wait_for_selector(".nickname", timeout=300000)
+        nickname = page.query_selector(".nickname").inner_text()
+        print(f"Reporter{i}已登录，昵称:", nickname)
+
+        # 保存最新状态
+        context.storage_state(path=os.path.join('model', f'reporter{i}.json') )
+
+        context.close()
+        browser.close()
+        playwright.stop()
+
+
